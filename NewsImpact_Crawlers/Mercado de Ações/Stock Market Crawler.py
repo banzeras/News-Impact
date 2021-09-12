d7efb11a552410c0a51d87717f6cc17ea9de3832
Emanuel Elias <eng.emanuel.elias@gmail.com>
2021-09-12 02:39:29 -0300

    Add files via upload

--- NewsImpact_Crawlers/Mercado de Ações/Stock Market Crawler.py
+++ NewsImpact_Crawlers/Mercado de Ações/Stock Market Crawler.py
@@ -4,38 +4,16 @@ import pandas as pd
 from pandas_datareader import data as dreader 
 import matplotlib.pyplot as plt
 from datetime import datetime
-"""
-Formato de datas aceito pelo datareader estão no Padrão Americano: Mês-Dia-ANO ou ANO-MÊS-DIA
-Para evitar confusão decidi adotar, o padrão ANO-MÊS-DIA.
-"""
-#ANO-MÊS-DIA
-#Mês-dia-ANO
-#data_inicial = '2015-05-02'
-#data_final = '2017-11-30'
 
 data_inicial =  datetime(2015,5,2)
 data_final =  datetime(2017,11,30)
-"""
-empresas_tickers = pd.read_excel("Equipamentos e Serviços.xlsx")
 
-for empresa in empresas_tickers["Empresas"]: 
-    print(f"{empresa}:")
-    df = dreader.DataReader(f"{empresa}.SA", data_source='yahoo', start=data_inicial, end=data_final)
-    print(df.head(4), "\n")
-    df.to_csv(f"{empresa}.SA.csv", encoding='utf-8')
 
-3.SA',
-
-"""
-tickers = ['VALE3.SA']
-
-#for i in tickers:
-    #dreader.DataReader(i,'yahoo',start=datetime(2015,5,2),end=datetime(2017,11,30)).to_csv(i+'.csv')
+tickers = pd.read_csv("Equipamentos e Serviços.csv")
 
 for i in tickers:
     try:
-        dreader.DataReader(i, data_source='yahoo',start=data_inicial,end=data_final).to_csv(i+'.csv')
+        dreader.DataReader(i+'.SA', data_source='yahoo',start=data_inicial,end=data_final).to_csv(i+'.csv')
     except KeyError: #Exception caso a data da existência da ação não for compatível com o intervalo de tempo limitado.
+        print(" A ação " + i +' não se encontra no intervalo de tempo pesquisado.')
         pass
-
-    
