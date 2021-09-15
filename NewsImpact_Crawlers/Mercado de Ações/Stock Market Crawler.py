b0ca0564c53c25350d2509adbb141b4956ac2bd3
Emanuel Elias <eng.emanuel.elias@gmail.com>
2021-09-15 10:51:21 -0300

    Update Stock Market Crawler.py

--- NewsImpact_Crawlers/Mercado de Ações/Stock Market Crawler.py
+++ NewsImpact_Crawlers/Mercado de Ações/Stock Market Crawler.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 import pandas as pd 
 from pandas_datareader import data as dreader 
-import matplotlib.pyplot as plt
 from datetime import datetime
 
 data_inicial =  datetime(2015,5,2)
@@ -13,7 +14,12 @@ tickers = pd.read_csv("Equipamentos e Serviços.csv")
 
 for i in tickers:
     try:
-        dreader.DataReader(i+'.SA', data_source='yahoo',start=data_inicial,end=data_final).to_csv(i+'.csv')
-    except KeyError: #Exception caso a data da existência da ação não for compatível com o intervalo de tempo limitado.
-        print(" A ação " + i +' não se encontra no intervalo de tempo pesquisado.')
+        dreader.DataReader(i+'.SA', data_source='yahoo',
+        start=data_inicial,end=data_final).to_csv(i+'.csv')
+        
+    except KeyError: #Exceção para caso a data da existência da ação
+    #Não for compatível com o intervalo de tempo limitado.
+    
+        print(' A ação ' + i +
+        ' não se encontra no intervalo de tempo pesquisado.')
         pass
